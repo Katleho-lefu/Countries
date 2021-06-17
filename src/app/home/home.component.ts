8b180d4f09b1082561b9cb4cdad0753f64eca9fd
Victor Lefu <victor@thedigitalacademy.co.za>
2021-06-17 12:05:03 +0200

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { CountryserviceService } from '../services/countryservice.service';
 
 @Component({
